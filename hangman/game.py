60820079b62eecf757a52b566b1106b2823364a8
Dan Tembe <31318833+dtembe@users.noreply.github.com>
2017-11-13 12:15:27 -0600

    Committing GuessAttempt Class.

--- hangman/game.py
+++ hangman/game.py
@@ -1,8 +1,27 @@
 from .exceptions import *
+import random
 
 
 class GuessAttempt(object):
-    pass
+    def __init__(self, alphabet, hit=None, miss=None):
+        self.alphabet = alphabet
+        self.hit = hit
+        self.miss = miss
+
+        if hit and miss:
+            raise InvalidGuessAttempt()
+
+    def is_hit(self):
+        if self.hit == None:
+            self.hit = False
+
+        return self.hit
+
+    def is_miss(self):
+        if self.miss == None:
+            self.miss = False
+
+        return self.miss
 
 
 class GuessWord(object):
